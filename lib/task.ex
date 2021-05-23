5c93abf716a34218ad2f5026bc321818975b3b99
Grant Horner <h.grant.horner@gmail.com>
2021-05-23 11:55:47 -0400

    reformatted and fixed optional parameters when creating tasks

--- lib/task.ex
+++ lib/task.ex
@@ -1,41 +1,56 @@
 defmodule Tasks.Task do
-	defstruct [:name, id: UUID.uuid4(), description: "", created_at: Date.utc_today(), assigned_to: "Unassigned"]
+  alias __MODULE__
 
-	@type keys :: [description: String.t(), created_at: Date.t(), assigned_to: String.t()]
+  defstruct [
+    :name,
+    status: "To do",
+    id: UUID.uuid4(),
+    description: "",
+    created_at: Date.utc_today(),
+    assigned_to: "Unassigned"
+  ]
 
-	@type t :: %__MODULE__{
-		name: String.t(),
-		description: String.t(),
-		assigned_to: String.t(),
-		created_at: Date.t()
-	}
+  @type t :: %Task{
+          name: String.t(),
+          status: String.t(),
+          id: UUID.t(),
+          description: String.t(),
+          created_at: Date.t(),
+          assigned_to: String.t()
+        }
 
-	@doc """
-	Creates a new task.
+  @not_modifiable [:__struct__, :id]
 
-	Returns the task.
+  @doc """
+  Creates a new task.
 
-	## Examples
+  Returns the task.
 
-		iex> Tasks.Task.new("a name", assigned_to: "you")
-		%Task{name: "a name", description: "", created_at: Date.utc_today(), assigned_to: "you"}
-	
-	"""
-	@spec new(String.t(), keys) :: t()
-	def new(name, opts \\ []) do
-		%__MODULE__{name: name}
-		|> try_add(opts, :description)
-		|> try_add(opts, :assigned_to)
-		|> try_add(opts, :created_at)
-	end
+  ## Examples
 
+  	iex> Tasks.Task.new("a name", assigned_to: "you")
+  	%Task{name: "a name", status: "To do", description: "", created_at: Date.utc_today(), assigned_to: "you"}
 
-	@spec try_add(t(), keys, atom()) :: t()
-	defp try_add(%__MODULE__{} = task, opts, key) do
-		if Keyword.has_key?(opts, key) do
-			%{task | key => Keyword.get(opts, key)}
-		else
-	 		task
-		end
-	end
+  """
+  @spec new(String.t(), key: any()) :: t()
+  def new(name, opts \\ []) do
+    opts = Enum.filter(opts, fn {key, _} -> not Enum.member?(@not_modifiable, key) end)
+
+    %Task{name: name}
+    |> apply_opts(opts)
+  end
+
+  @spec apply_opts(t(), key: any()) :: t()
+  defp apply_opts(%Task{} = task, [{key, value} | rest]) do
+    apply_opts(%{task | key => value}, rest)
+  end
+
+  @spec apply_opts(t(), []) :: t()
+  defp apply_opts(%Task{} = task, []) do
+    task
+  end
+
+  defp try_add(%Task{} = task, key, value) do
+    %{task | key => value}
+  end
 end
--- test/task_test.exs
+++ test/task_test.exs
@@ -1,21 +1,27 @@
 defmodule TaskTest do
-	use ExUnit.Case, async: true
-	alias Tasks.Task
-	doctest Task
+  use ExUnit.Case, async: true
+  alias Tasks.Task
+  doctest Task
 
-	@today Date.utc_today()
+  @today Date.utc_today()
 
-	test "creates a new task" do
-		assert %Task{name: "a", description: "", assigned_to: "Unassigned", created_at: @today} = Task.new("a")
-	end
+  test "creates a new task" do
+    assert %Task{name: "a", description: "", assigned_to: "Unassigned", created_at: @today} =
+             Task.new("a")
+  end
 
-	test "new tasks have the correct date" do
-		task = Task.new("a")
-		assert task.created_at == Date.utc_today()
-	end
+  test "new tasks have the correct date" do
+    task = Task.new("a")
+    assert task.created_at == Date.utc_today()
+  end
 
-	test "can dynamically set fields" do
-		task = Task.new("a", description: "some description", assigned_to: "me")
-		assert %Task{description: "some description", assigned_to: "me"} = task
-	end
+  test "can dynamically set fields" do
+    task = Task.new("a", description: "some description", assigned_to: "me")
+    assert %Task{description: "some description", assigned_to: "me"} = task
+  end
+
+  test "can not set private fields" do
+    task = Task.new("a", id: "my id")
+    assert task.id != "my id"
+  end
 end
